a938c3a49c708313af09fb4a25d16b00f80d75b9
Qi1 <imqi1@qq.com>
2024-02-06 13:48:46 +0800

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -12,31 +12,11 @@ const Footer = () => {
 
   return (
     <footer id="footer">
-      <div className="social">
-        <CustomLink
-          iconDom={<GithubOne />}
-          to={`https://github.com/${githubName}/`}
-        />
-        <CustomLink iconDom={<Home />} to={homeUrl} />
-        <CustomLink iconDom={<Mail />} to={`mailto:${emailUrl}`} />
-      </div>
       <div className="text">
-        <p>
-          <CustomLink
-            text={Package.alia}
-            to="https://github.com/imsyy/site-status"
-          />
-          &nbsp;Version&nbsp;{Package.version}
-        </p>
-        <p>
-          基于&nbsp;
-          <CustomLink to="https://uptimerobot.com/" text="UptimeRobot" />
-          &nbsp;接口&nbsp;|&nbsp;检测频率 5 分钟
-        </p>
         <p>
           Copyright&nbsp;&copy;&nbsp;2020&nbsp;-&nbsp;{new Date().getFullYear()}
           &nbsp;
-          <CustomLink to="https://www.imsyy.top/" text="無名" />
+          <CustomLink to="https://imqi1.com/" text="ImQi1" />
           {siteIcp ? (
             <React.Fragment>
               &nbsp;|&nbsp;
